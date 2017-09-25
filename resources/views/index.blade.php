58a4ee09794aefa835717146d15128c60227a72a
cuong <nguyenxuancuongkth@gmail.com>
2017-09-25 07:20:19 +0700

    fix index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -79,11 +79,11 @@
 <!-- /.login-box -->
 
 <!-- jQuery 3 -->
-<script src="../../bower_components/jquery/dist/jquery.min.js"></script>
+<script src="{{asset('lte/bower_components/jquery/dist/jquery.min.js')}}"></script>
 <!-- Bootstrap 3.3.7 -->
-<script src="../../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="{{ asset('lte/bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
 <!-- iCheck -->
-<script src="../../plugins/iCheck/icheck.min.js"></script>
+<script src="{{ asset('lte/plugins/iCheck/icheck.min.js') }}"></script>
 <script>
     $(function () {
         $('input').iCheck({
